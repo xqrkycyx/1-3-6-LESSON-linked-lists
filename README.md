720a7baa987553aaca8b1b039608ea1ca2175a87
Will Williams <wwilliams@indeed.com>
2024-02-27 14:09:43 -0800

    Adds file to use/instantiate LL

---
+++ README.md
@@ -0,0 +1,7 @@
+Run the code using the command `node useLinkedList.js` - You will see the following output:
+
+```
+LinkedList {
+  head: Node { value: 2, next: Node { value: 'One', next: null } }
+}
+```
---
+++ useLinkedList.js
@@ -0,0 +1,10 @@
+const LinkedList = require("./linkedList");
+
+const linkedList = new LinkedList();
+
+linkedList.insertAtHead("One");
+linkedList.insertAtHead(2);
+
+console.log(linkedList);
+
+// RUN THIS USING "node useLinkedList.js"
